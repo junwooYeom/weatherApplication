3cd6a9bc78d92533482dc7705f5f641430707433
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-04-30 19:39:59 +0900

    [FEAT] 액티비티 뷰 설정 및 어댑터 연결.

--- app/src/main/java/com/junwooyeom/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/junwooyeom/weatherapplication/MainActivity.kt
@@ -2,10 +2,32 @@ package com.junwooyeom.weatherapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.junwooyeom.weatherapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val adapter by lazy {
+        WeatherAdapter()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.rvWeather.layoutManager = LinearLayoutManager(this)
+        binding.rvWeather.adapter = adapter
+        binding.rvWeather.addItemDecoration(
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        )
     }
 }
