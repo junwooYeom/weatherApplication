736048b0f007e4329304a95c4c5c420253925bcc
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-04-30 13:06:01 +0900

    [FEAT] WeatherItem 을 가지고 오는 UseCase 추가.

---
+++ app/src/main/java/com/junwooyeom/weatherapplication/GetWeatherUseCase.kt
@@ -0,0 +1,16 @@
+package com.junwooyeom.weatherapplication
+
+import javax.inject.Inject
+
+class GetWeatherUseCase @Inject constructor(
+    private val weatherRepository: WeatherRepository
+){
+    suspend operator fun invoke() : List<WeatherItem> {
+        val weatherList : MutableList<WeatherItem> = mutableListOf()
+        weatherRepository.getWeather().forEach { list ->
+            val response = weatherRepository.getWeatherCity(list.id)
+            weatherList.add(WeatherItem(list.title, response[0], response[1]))
+        }
+        return weatherList
+    }
+}
